db78171c2cd8f18458baa752759be35e8798b290
Sztyup <sztyup@gmail.com>
2018-03-17 00:26:42 +0100

    Refactor middleware

--- src/Impostor/Middleware/Impersonate.php
+++ src/Impostor/Middleware/Impersonate.php
@@ -6,12 +6,11 @@ use Closure;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Str;
 use Sztyup\Impostor\ImpersonationManager;
 
 class Impersonate
 {
-    use InjectorMiddleware;
-
     const SESSION_KEY = '_nexus_impersonate';
 
     /** @var Factory */
@@ -41,12 +40,32 @@ class Impersonate
 
     protected function injectImpersonateBar(Response $response)
     {
-        $view = $this->viewFactory->make('nexus::impersonate', [
+        $view = $this->viewFactory->make('impostor::impersonateBar', [
             'user' => $this->manager->getImpersonatedUser()
         ])->render();
 
         $content = preg_replace("/<body([^>])*>/", "\\0\n$view", $response->getContent());
 
         $response->setContent($content);
+    }
+
+    protected function shouldInject($response)
+    {
+        // Not a normal response, probably a redirect
+        if (!$response instanceof Response) {
+            return false;
+        }
+
+        // Explicit redirect response
+        if ($response->isRedirection()) {
+            return false;
+        }
+
+        // Only inject html responses
+        if (!Str::contains($response->headers->get('Content-Type'), 'html')) {
+            return false;
+        }
+
+        return true;
     }
 }
